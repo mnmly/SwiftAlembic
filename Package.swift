b290c4825dce42930b5caadce329f47fef4739b6
Hiroaki Yamane <i.am@mnmly.com>
2026-05-04 22:10:17 +0100

    Using remote xcframeworks

--- Package.swift
+++ Package.swift
@@ -6,12 +6,14 @@ let package = Package(
     name: "SwiftAlembic",
     platforms: [.macOS(.v13)],
     products: [
-        .library(name: "SwiftAlembic", targets: ["SwiftAlembic"])
+        .library(name: "SwiftAlembic", targets: ["SwiftAlembic"]),
+        .executable(name: "Examples", targets: ["Examples"])
     ],
     targets: [
         .binaryTarget(
             name: "Alembic",
-            path: "Frameworks/Alembic.xcframework"
+            url: "https://github.com/mnmly/alembic-xcframework-builder/releases/download/1.81.11-fix/Alembic.xcframework.zip",
+            checksum: "4a10cf92c8c4a542644c6317d9487736c277b20bd19bf43ff780318bfa7fd46b"
         ),
         .target(
             name: "CAlembic",
@@ -24,6 +26,14 @@ let package = Package(
         .target(
             name: "SwiftAlembic",
             dependencies: ["CAlembic"],
+            swiftSettings: [
+                .interoperabilityMode(.Cxx)
+            ]
+        ),
+        .executableTarget(
+            name: "Examples",
+            dependencies: ["SwiftAlembic"],
+            path: "Examples",
             swiftSettings: [
                 .interoperabilityMode(.Cxx)
             ]
